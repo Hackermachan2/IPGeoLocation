53204d80cf4ee6b8c452216395e00e69e9bb050c
hackermachan <88127923+Hackermachan2@users.noreply.github.com>
2021-07-31 21:50:24 +0530

    Update IpGeoLocationLib.py

--- core/IpGeoLocationLib.py
+++ core/IpGeoLocationLib.py
@@ -4,7 +4,8 @@
 """
     This file is part of IPGeoLocation tool.
     Copyright (C) 2015-2016 @maldevel
-    https://github.com/maldevel/IPGeoLocation
+    Modify    (M) 2020-2021 @Hackermachan
+    https://github.com/Hackermachan2/IPGeoLocation
     
     IPGeoLocation - Retrieve IP Geolocation information 
     Powered by http://ip-api.com
